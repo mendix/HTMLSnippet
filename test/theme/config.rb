0f05083c85852c4d9cbaa9f487b39412bc305091
Melvin Grootenboers <melvin.grootenboers@mendix.com>
2015-10-19 11:18:16 +0200

    New Testproject

---
+++ test/theme/config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'styles/css'
+sass_dir = 'styles/sass'
+relative_assets = true
+line_comments = false
+sourcemap = false
+output_style = :expanded
