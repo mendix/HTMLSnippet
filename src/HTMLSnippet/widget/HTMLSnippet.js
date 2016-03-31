87a04d92a4742acc553795282aff25a089a477b5
Robert van 't Hof <robert.van.thof@mendix.com>
2016-03-31 15:32:18 +0200

    Converted the tabs/spaces mess to use tabs Added Refresh on context change option

--- src/HTMLSnippet/widget/HTMLSnippet.js
+++ src/HTMLSnippet/widget/HTMLSnippet.js
@@ -2,101 +2,114 @@
 /*global mx, mxui, mendix, dojo, require, console, define, module, document*/
 
 require([
-  "dojo/_base/declare",
-  "mxui/widget/_WidgetBase",
+	"dojo/_base/declare",
+	"mxui/widget/_WidgetBase",
 
-  "mxui/dom",
-  "dojo/dom-style",
-  "dojo/dom-attr",
-  "dojo/dom-construct",
-  "dojo/_base/lang",
-  "dijit/layout/LinkPane"
-], function (declare, _WidgetBase, dom, domStyle, domAttr, domConstruct, lang, linkPane) {
-  "use strict";
+	"mxui/dom",
+	"dojo/dom-style",
+	"dojo/dom-attr",
+	"dojo/dom-construct",
+	"dojo/_base/lang",
+	"dijit/layout/LinkPane"
+], function(declare, _WidgetBase, dom, domStyle, domAttr, domConstruct, lang, linkPane) {
+	"use strict";
 
-  return declare("HTMLSnippet.widget.HTMLSnippet", [_WidgetBase], {
+	return declare("HTMLSnippet.widget.HTMLSnippet", [_WidgetBase], {
 
-    postCreate: function () {
-      var external = this.contentsPath !== "" ? true : false;
-      this._setupEvents();
+		postCreate: function() {
+			this._setupEvents();
 
-      switch (this.contenttype) {
-        case "html":
-          if (external) {
-            new linkPane({
-              preload: true,
-              loadingMessage: "",
-              href: this.contentsPath,
-              onDownloadError: function(){
-                console.log("Error loading html path");
-              }
-            }).placeAt(this.domNode.id).startup();
-          } else {
-            domStyle.set(this.domNode, {
-              "height": "auto",
-              "width": "100%",
-              "outline": 0
-            });
+			if (!this.refreshOnContextChange) {
+				this.executeCode();
+			}
+		},
 
-            domAttr.set(this.domNode, "style", this.style); // might override height and width
-            var domNode = domConstruct.create("div", { innerHTML: this.contents });
-            domConstruct.place(domNode, this.domNode, "only");
-          }
-          break;
+		executeCode: function() {
+			var external = this.contentsPath !== "" ? true : false;
+			switch (this.contenttype) {
+				case "html":
+					if (external) {
+						new linkPane({
+							preload: true,
+							loadingMessage: "",
+							href: this.contentsPath,
+							onDownloadError: function() {
+								console.log("Error loading html path");
+							}
+						}).placeAt(this.domNode.id).startup();
+					} else {
+						domStyle.set(this.domNode, {
+							"height": "auto",
+							"width": "100%",
+							"outline": 0
+						});
 
-        case "js":
-        case "jsjQuery":
-          if (external) {
-            var scriptNode = document.createElement("script"),
-                intDate = +new Date();
+						domAttr.set(this.domNode, "style", this.style); // might override height and width
+						var domNode = domConstruct.create("div", { innerHTML: this.contents });
+						domConstruct.place(domNode, this.domNode, "only");
+					}
+					break;
 
-              scriptNode.type = "text/javascript";
-              scriptNode.src = this.contentsPath + "?v=" + intDate.toString();
+				case "js":
+				case "jsjQuery":
+					if (external) {
+						var scriptNode = document.createElement("script"),
+							intDate = +new Date();
 
-              domConstruct.place(scriptNode, this.domNode, "only");
-          } else {
-            if (this.contenttype == "jsjQuery") {
-              require(["HTMLSnippet/lib/jquery-1.11.3"], lang.hitch(this, this.evalJs));
-            } else {
-              this.evalJs();
-            }
-          }
-          break;
-      }
-    },
+						scriptNode.type = "text/javascript";
+						scriptNode.src = this.contentsPath + "?v=" + intDate.toString();
 
-    _setupEvents: function () {
-      if (this.onclickmf) {
-        this.connect(this.domNode, "click", this._executeMicroflow);
-      }
-    },
+						domConstruct.place(scriptNode, this.domNode, "only");
+					} else {
+						if (this.contenttype == "jsjQuery") {
+							require(["HTMLSnippet/lib/jquery-1.11.3"], lang.hitch(this, this.evalJs));
+						} else {
+							this.evalJs();
+						}
+					}
+					break;
+			}
+		},
 
-    _executeMicroflow: function () {
-      if (this.onclickmf) {
-        mx.data.action({
-          store: {
-            caller: this.mxform
-          },
-          params: {
-            actionname: this.onclickmf
-          },
-          callback: function () {
-              // ok
-          },
-          error: function () {
-              // error
-          }
-        });
-      }
-    },
+		update: function(obj, callback) {
+			if (this.refreshOnContextChange) {
+				this.executeCode();
+			}
+			callback();
+		},
 
-    evalJs: function () {
-      try {
-        eval(this.contents + "\r\n//# sourceURL=" + this.id + ".js");
-      } catch (e) {
-        domConstruct.place("<div class=\"alert alert-danger\">Error while evaluating javascript input: " + e + "</div>", this.domNode, "only");
-      }
-    }
+		_setupEvents: function() {
+			if (this.onclickmf) {
+				this.connect(this.domNode, "click", this._executeMicroflow);
+			}
+		},
 
-  });
+		_executeMicroflow: function() {
+			if (this.onclickmf) {
+				mx.data.action({
+					store: {
+						caller: this.mxform
+					},
+					params: {
+						actionname: this.onclickmf
+					},
+					callback: function() {
+						// ok
+					},
+					error: function() {
+						// error
+					}
+				});
+			}
+		},
+
+		evalJs: function() {
+			try {
+				eval(this.contents + "\r\n//# sourceURL=" + this.id + ".js");
+			} catch (e) {
+				domConstruct.place("<div class=\"alert alert-danger\">Error while evaluating javascript input: " + e + "</div>", this.domNode, "only");
+			}
+		}
+
+	});
 });
