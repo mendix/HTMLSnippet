9115f15991f8b26ded6e0aa7acd5aa9a5a734774
Eric Tieniber <tieniber@gmail.com>
2016-11-07 14:51:08 -0500

    added offline widget using same source code

---
+++ src/HTMLSnippet/widget/HTMLSnippetOffline.js
@@ -0,0 +1,9 @@
+require([
+    "dojo/_base/declare", "HTMLSnippet/widget/HTMLSnippet"
+], function(declare, _htmlSnippetWidget) {
+    return declare("HTMLSnippet.widget.HTMLSnippetOffline", [_htmlSnippetWidget], {
+    })
+});
+require(["HTMLSnippet/widget/HTMLSnippetOffline"], function() {
+    "use strict";
+});
